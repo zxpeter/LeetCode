f74fe436aa45eb0fdfb2cdd97c6a994f52677a13
zxpeter <408796230@qq.com>
2018-09-26 16:54:53 +0800

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -22,3 +22,21 @@ if __name__ == '__main__':
     p_list = [1,4,2,3,5,2]
     quick_sort(p_list, 0, len(p_list)-1)
     print(p_list)
+
+    
+def quick_sort(array, left, right):
+    if left >= right:
+        return
+    low = left
+    high = right
+    key = array[low]
+    while left < right:
+        while left < right and array[right] > key:
+            right -= 1
+        array[left] = array[right]
+        while left < right and array[left] <= key:
+            left += 1
+        array[right] = array[left]
+    array[right] = key
+    quick_sort(array, low, left - 1)
+    quick_sort(array, left + 1, high)
