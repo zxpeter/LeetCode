f1ecd110b4d822ac238b56f92eeb2ed11c64e484
Coding_Monkey <408796230@qq.com>
2018-10-22 15:41:30 +0800

    Update Sort_MergeSort.py

--- Sort_MergeSort.py
+++ Sort_MergeSort.py
@@ -1,3 +1,10 @@
+def bubbleSort(nums):
+    for i in range(len(nums)-1):    # 这个循环负责设置冒泡排序进行的次数
+        for j in range(len(nums)-i-1):  # ｊ为列表下标
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+    return nums
+
 def merge(a, b):
     c = []
     h = j = 0
