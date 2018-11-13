ab0daf322a66f92619ed7bfac0e219f298dae914
Coding_Monkey <408796230@qq.com>
2018-11-13 22:53:34 +0800

    Update Sort_MergeSort.py

--- Sort_MergeSort.py
+++ Sort_MergeSort.py
@@ -5,6 +5,26 @@ def bubbleSort(nums):
                 nums[j], nums[j+1] = nums[j+1], nums[j]
     return nums
 
+# merge 2 sorted array use in-place algorithm
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+Output: [1,2,2,3,5,6]
+    
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        if n > 0:
+            nums1[:n] = nums2[:n]
+
+            
+# merge sort
 def merge(a, b):
     c = []
     h = j = 0
