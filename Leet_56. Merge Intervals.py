823ade248ab096f100df34775ced0abfa57d5511
Coding_Monkey <408796230@qq.com>
2018-11-25 10:46:15 +0800

    Create Leet_56. Merge Intervals.py

---
+++ Leet_56. Merge Intervals.py
@@ -0,0 +1,24 @@
+56. Merge Intervals
+Input: [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+# Definition for an interval.
+# class Interval:
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution:
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+        res = []
+        for i in sorted(intervals, key=lambda x: x.start):
+            if res and i.start <= res[-1].end:
+                res[-1].end = max(res[-1].end, i.end)
+            else:
+                res.append(i)
+        return res
